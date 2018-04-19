741f353a3ae2e7878dcf4601707853e15a40cd77
Michael Boroski <mjboroski@gmail.com>
2018-04-19 21:35:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,9 @@
 katz_deli = []
 
 def line(katz_deli)
- katz_deli.length==0 ? puts "The line is currently empty.": nil
+ if katz_deli.length==0
+   puts "The line is currently empty."
+ end
 
 end
 
