b832650f52eac5f35fdcdbff7345ae036dec401f
Michael Boroski <mjboroski@gmail.com>
2018-04-19 22:38:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,5 +20,6 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  katz_deli==nil ? puts "There is nobody waiting to be served!" : puts "Currently serving #{katz_deli.unshift}"
+  katz_deli==nil ? statement="There is nobody waiting to be served!" : statement="Currently serving #{katz_deli.unshift}"
+  puts statement
 end
