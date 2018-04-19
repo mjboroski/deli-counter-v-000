47c44150e5036ce0920d6962aa5bebef2c677781
Michael Boroski <mjboroski@gmail.com>
2018-04-19 21:34:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+ katz_deli.length==0 ? puts "The line is currently empty."
+   
+end
+
+def take_a_number(katz_deli, name)
+
+end
+
+def now_serving(katz_deli)
+
+end
