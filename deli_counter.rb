f844ec19d62ab82b85a42e13f3cd1eb6604802ee
Michael Boroski <mjboroski@gmail.com>
2018-04-19 22:26:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,17 +7,16 @@ def line(katz_deli)
    x=1
    suffix = ":"
    while x<=katz_deli.length
-
      suffix<<" #{x}. #{katz_deli[x-1]}"
      x+=1
    end
  end
  puts "The line is currently"<<suffix
-
 end
 
 def take_a_number(katz_deli, name)
-
+  katz_deli.push(name)
+  return "Welcome, #{name}. You are number #{katz_deli.length} in line"
 end
 
 def now_serving(katz_deli)
